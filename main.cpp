1c54622c4b5ed05a89785eb9c458bc1f80dac983
il8677 <is48le50@isdedu.de>
2018-03-17 19:49:14 +0100

    Added basic device functionality

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 using namespace rlutil;
 using namespace std;
 
+const int universeSize = 512;
+
+const string banner = "Eyepatch";
 const string seperator=",,,,";
 
 vector<string> split(string str, string sep){
@@ -56,23 +59,128 @@ public:
     }
 };
 
-int main(){
+vector<DMXDevice> devices;
+void saveDevices(){
+    ofstream outfile;
 
-    //tests
-    /*
-    DMXDevice testDevice(NULL);
+    outfile.open("devices.icsv", ios::out);
+
+    for(int i =0 ; i < devices.size(); i++){
+        cout << "Saving ";
+        devices[i].printInfo();
+        cout <<endl;
+        devices[i].serialise(&outfile);
+    }
+
+    outfile.close();
+
+    cout << "Devices saved, press any key to continue...";
+    anykey();
+}
+
+void loadDevices(){
     ifstream infile;
     infile.open("devices.icsv", ios::in);
+
     string input;
-    infile>>input;
     vector<string> splitinput;
+
+    infile>>input;
     splitinput = split(input, "\n");
-    testDevice.deserialise(splitinput[0]);
-    testDevice.printInfo();
-    infile.close();*/
-    /*ofstream outfile;
-    outfile.open("devices.icsv", ios::out|ios::app);
-    testDevice.serialise(&outfile);
-    outfile.close();*/
+    if(splitinput.size()==0){
+        cout << splitinput.size();
+        return;
+    }
+    for(int i = 0; i < splitinput.size(); i++){
+        devices.push_back(DMXDevice(NULL));
+        devices[i].deserialise(splitinput[i]);
+    }
+}
+
+void printAllDevices(){
+    for(int i = 0; i < devices.size(); i++){
+        cout << i<< ": ";
+        devices[i].printInfo();
+    }
+    cout<<"Press any key to continue..."<<endl;
+    getch();
+}
+
+void newDevice(){
+    string name;
+    int quantity;
+    int footprint;
+
+    cout << "Device addition mode:"<<endl;
+    cout << "Name: ";
+    cin >> name;
+    cout <<"Quantity: ";
+    cin >> quantity;
+    cout << "DMX Footprint: ";
+    cin >> footprint;
+
+    devices.push_back( DMXDevice(footprint,name,quantity));
+}
+
+void editDevice(){
+    int id;
+
+    cout << "Device edit mode:"<<endl;
+    printAllDevices();
+    cout << "Enter device id: ";
+
+    cin >> id;
+    anykey();
+    cls();
+    cout << "Device edit mode:"<<endl;
+
+    cout << "Editing ";
+    devices[id].printInfo();
+
+    cout << "q. Edit quantity\nn. Edit name\nf. Edit footprint\n";
+    char in = getch();
+    cls();
+    cout << "Device edit mode:"<<endl;
+    if(in == 'q'){
+        cout << "Enter new quantity: ";
+        int n;
+        cin >> n;
+
+        devices[id].quantity=n;
+    }else if (in == 'n'){
+        cout << "Enter new name: ";
+        string n;
+        cin>> n;
+
+        devices[id].name=n;
+    }else if (in == 'f'){
+        cout << "Enter new DMX footprint: ";
+        int n;
+        cin >> n;
+
+        devices[id].footprint=n;
+    }
+}
+
+int main(){
+    cout << banner;
+    loadDevices();
+    cout <<endl<<endl;
+    while (true){
+        cout <<"n. new device\nl. list device\ns. save devices\ne. edit device\n";
+
+        char in = getch();
+        cls();
+        if(in == 'n'){
+            newDevice();
+        }else if (in == 'l'){
+            printAllDevices();
+        }else if (in == 's'){
+            saveDevices();
+        }else if (in == 'e'){
+            editDevice();
+        }
+        cls();
+    }
     return 0;
 }
